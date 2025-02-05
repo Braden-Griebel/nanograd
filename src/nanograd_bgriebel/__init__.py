826c44d92eac9d063def5337bc2cee3bce1d3a36
Braden Griebel <bgriebel@uw.edu>
2025-02-04 22:50:41 -0800

    Reformat

--- src/nanograd_bgriebel/__init__.py
+++ src/nanograd_bgriebel/__init__.py
@@ -2,10 +2,9 @@ from importlib.metadata import version
 
 __author__ = "Braden Griebel"
 __version__ = version("nanograd_bgriebel")
-__all__ = ["engine", "Value"]
+__all__ = ["engine", "nn", "Value", "Module", "Neuron", "Layer", "MultiLayerPerceptron"]
 
 # Package Imports
 from nanograd_bgriebel._core import engine, nn
 from nanograd_bgriebel._core.engine import Value
 from nanograd_bgriebel._core.nn import Module, Neuron, Layer, MultiLayerPerceptron
-
