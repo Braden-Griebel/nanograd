5e073b48cc18f54044cb8d51792ee89df9d1f215
Braden Griebel <bgriebel@uw.edu>
2025-02-04 21:00:22 -0800

    Added tests for creating a multilayer perceptron

--- src/nanograd_core/nn.h
+++ src/nanograd_core/nn.h
@@ -151,7 +151,7 @@ public:
      */
     MultiLayerPerceptron(int nin, std::vector<int> nouts) {
         this->layers.emplace_back(nin, nouts[0], 0 != nouts.size() - 1);
-        for (int idx = 1; idx < nouts.size(); ++idx) {
+        for (int idx = 0; idx < nouts.size()-1; ++idx) {
             this->layers.emplace_back(nouts[idx], nouts[idx + 1], idx != nouts.size() - 1);
         }
     }
--- tests/test_nn.cpp
+++ tests/test_nn.cpp
@@ -75,4 +75,42 @@ TEST_CASE("Creating Modules", "[nn]") {
             CHECK_THAT(param.get_grad(), Catch::Matchers::WithinAbs(0.0, margin));
         }
     }
+
+    SECTION("Creating MultiLayerPerceptrons") {
+        std::vector layerSizes {5,5,4,3};
+        MultiLayerPerceptron testMultiLayerPerceptron{4,layerSizes};
+        // Margin for floating point
+        const double margin = 0.0000001;
+
+        /* The layers have:
+         * -> 5 neurons * (4 weights + 1 bias) = 25 total
+         * -> 5 neurons * (5 weights + 1 bias) = 30 total
+         * -> 4 neurons * (5 weights + 1 bias) = 24 total
+         * -> 3 neurons * (4 weights + 1 bias) = 15 total
+         * Overall: 74 total parameters
+         */
+        CHECK(testMultiLayerPerceptron.get_parameters().size() == 94);
+
+        // Check that the Layer can be called
+        const std::vector<Value> inputs = {Value{1.0},Value{1.0},Value{1.0},Value{1.0},};
+        const std::vector<Value> outputs = testMultiLayerPerceptron(inputs);
+
+        // The size of the last layer is 3, so there should be 3 outputs
+        CHECK(outputs.size() == 3);
+
+        // Check that the gradients can be computed
+        for (auto& out: outputs) {
+            out.backwards();
+        }
+        for (auto& param: testMultiLayerPerceptron.get_parameters()) {
+            // Due to the ReLU all the gradients should be non-negative
+            CHECK(param.get_grad()>=0.0);
+        }
+        // Check that zeroing the gradients works
+        testMultiLayerPerceptron.zero_grad();
+        for (auto& param: testMultiLayerPerceptron.get_parameters()) {
+            CHECK_THAT(param.get_grad(), Catch::Matchers::WithinAbs(0.0, margin));
+        }
+
+    }
 }
